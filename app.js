1c9c622f93c779648aa8bb99edd85f112cc04974
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-15 10:39:50 +0800

    testing

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.get('/', function (req, res) {
   res.send('testing');
 });
 
-app.post('/insert-url-and-phone', function (req, res) {
-  let url = req.param('url');
-  let phone = req.param('phone');
-  let urlId = req.param('url_id');
-  res.charset = 'utf-8';
-  res.send(url + ' ' + phone + ' ' + urlId);
-});
+// app.post('/insert-url-and-phone', function (req, res) {
+//   let url = req.param('url');
+//   let phone = req.param('phone');
+//   let urlId = req.param('url_id');
+//   res.charset = 'utf-8';
+//   res.send(url + ' ' + phone + ' ' + urlId);
+// });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
