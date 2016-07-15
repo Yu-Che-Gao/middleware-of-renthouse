90a38546cc99bf63839c82f2d5f294cb5adb89cd
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-15 10:47:28 +0800

    增加根目錄

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const request = require('request');
 const port = process.env.PORT || 80;
 
+app.get('/', function(req, res) {
+  res.send('you have no right to access this page.');
+});
+
+
 app.get('/insert-url-and-phone', function (req, res) {
   let url = req.params.url;
   let phone = req.params.phone;
