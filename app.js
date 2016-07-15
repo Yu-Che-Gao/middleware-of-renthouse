c70be7882395a0a5da89055de124da57e26585a1
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-15 17:46:08 +0800

    TODO: 連結爬蟲

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const bodyParser = require('body-parser');
-const lib = reqire('./lib.js');
+const lib = require('./lib.js');
 const port = process.env.PORT || 80;
 
 app.use(function (req, res, next) {
@@ -10,7 +10,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.send('you have no right to access this page.');
@@ -20,7 +20,7 @@ app.post('/insert-url-and-phone', function (req, res) {
   let url = req.body.url;
   let phone = req.body.phone;
   let urlId = req.body.url_id;
-  lib.insertDB(url, phone, urlId);
+  lib.insertDB(url, phone, urlId, res);
 });
 
 app.listen(port, function () {
--- lib.js
+++ lib.js
@@ -1,27 +1,47 @@
-exports.insertDB=function(url, phone, urlId) {
-  request.post({
-    headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    url: 'http://x.rce.tw/s/h3584935/insert_url_and_phone.php',
-    body: 'url=' + url + '&phone=' + phone + '&url_id=' + urlId
-  }, function (error, response, body) {
-    let bodyParser=JSON.parse(body);
-    switch(bodyParser.result){
-        case 1:
-            console.log('code 1: database has the same data.');
-            break;
-        case 2:
-            console.log('code 2: start to send request to disqus.');
-            //TODO: 查出所有有相童手機的人
-            //TODO: 去disqus把所有討論串爬回來
-            break;
+const request = require('request');
 
-        case 3:
-            console.log('code 3: database problem occurs.');
-            break;
-    }
-  });
+exports.insertDB = function (url, phone, urlId, res) {
+    request.post({
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'http://x.rce.tw/s/h3584935/insert_url_and_phone.php',
+        body: 'url=' + url + '&phone=' + phone + '&url_id=' + urlId
+    }, function (error, response, body) {
+        let bodyParser = JSON.parse(body);
+        switch (bodyParser.result) {
+            case '1':
+                console.log('code 1: database has the same data.');
+                selectDBByPhone(phone);
+                break;
+            case '2':
+                console.log('code 2: start to send request to disqus.');
+                selectDBByPhone(phone);
+                break;
+
+            case '3':
+                console.log('code 3: database problem occurs.');
+                break;
+        }
+    });
 }
 
 function sendDisqus(str) {
     console.log(str);
+}
+
+function selectDBByPhone(phone) {
+    request.post({
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'http://x.rce.tw/s/h3584935/select_where_phone.php',
+        body: 'phone=' + phone
+    }, function (error, response, body) {
+        let bodyParser = JSON.parse(body);
+        for (let i = 0; i < bodyParser.length; i++) {
+            console.log(bodyParser[i].url);
+            //selectDisqus(bodyParser[i].url);
+        }
+    });
+}
+
+function selectDisqus(url) {
+     //TODO: 去disqus把所有討論串爬回來
 }
