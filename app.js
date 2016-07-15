563db791b94e5218f6698028b4dd9658412808f1
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-15 10:45:17 +0800

    先改成get測試

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const app = express();
 const request = require('request');
 const port = process.env.PORT || 80;
 
-app.get('/', function (req, res) {
-  res.send('testing');
+app.get('/insert-url-and-phone', function (req, res) {
+  let url = req.params.url;
+  let phone = req.params.phone;
+  let urlId = req.params.url_id;
+  res.charset = 'utf-8';
+  res.send(url + ' ' + phone + ' ' + urlId);
 });
-
-// app.post('/insert-url-and-phone', function (req, res) {
-//   let url = req.param('url');
-//   let phone = req.param('phone');
-//   let urlId = req.param('url_id');
-//   res.charset = 'utf-8';
-//   res.send(url + ' ' + phone + ' ' + urlId);
-// });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
