647415b4d48078a041aa8af9582fe3d0a07ad5c4
Yu-Che-Gao <s0258037@gm.csmu.edu.tw>
2016-07-15 22:01:24 +0800

    加入送出json給disqus-drawler

--- lib.js
+++ lib.js
@@ -10,13 +10,12 @@ exports.insertDB = function (url, phone, urlId, res) {
         switch (bodyParser.result) {
             case '1':
                 console.log('code 1: database has the same data.');
-                selectDBByPhone(phone);
+                selectDBByPhone(phone, res);
                 break;
             case '2':
                 console.log('code 2: start to send request to disqus.');
-                selectDBByPhone(phone);
+                selectDBByPhone(phone, res);
                 break;
-
             case '3':
                 console.log('code 3: database problem occurs.');
                 break;
@@ -28,20 +27,28 @@ function sendDisqus(str) {
     console.log(str);
 }
 
-function selectDBByPhone(phone) {
+function selectDBByPhone(phone, res) {
     request.post({
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         url: 'http://x.rce.tw/s/h3584935/select_where_phone.php',
         body: 'phone=' + phone
     }, function (error, response, body) {
         let bodyParser = JSON.parse(body);
+        let urlArray = new Object();
         for (let i = 0; i < bodyParser.length; i++) {
-            console.log(bodyParser[i].url);
-            //selectDisqus(bodyParser[i].url);
+            urlArray[i].url = bodyParser[i].url;
         }
+
+        selectDisqus(urlArray, res);
     });
 }
 
-function selectDisqus(url) {
-     //TODO: 去disqus把所有討論串爬回來
+function selectDisqus(urlArray, res) {
+    request.post({
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        url: 'http://disqus-crawler.azurewebsites.net/listThreads',
+        body: 'urlJSON=' + JSON.stringify(urlArray)
+    }, function (error, response, body) {
+        res.send(body);
+    });
 }
